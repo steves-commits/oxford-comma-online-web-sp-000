326e727c750056295bbbfe0304e495838e37281c
Steven Topazio <sat@northpointprinting.com>
2020-05-13 15:57:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,5 @@ if array.size == 1
   return array[0]
   eslif array.size == 2 
   return array.join ("and")
-else return array[0..-2].join(',') + ", and"
+else return array[0..-2].join(',') + ", and "
 end
