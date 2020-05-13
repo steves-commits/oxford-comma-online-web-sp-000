72e3d14bc04b4d409967dd4f1742aec0cd4b2f18
Steven Topazio <sat@northpointprinting.com>
2020-05-13 16:03:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,6 @@ if array.size == 1
   return array[0]
   elsif array.size == 2 
   return array.join (" and ")
-else return array[0..-2].join(',') + ", and " + array[-1]
+else return array[0..-2].join(',') + ", and" + array[-1]
 end
 end
